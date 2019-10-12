031c59d2cb90177b4e89f8d2f0e82c834d102e74
Pablo Cardona <pablocardona@jarabesoft.com>
2019-10-11 19:38:40 -0500

    Clase Ususario y Conexion con la db

--- ATM2.0/Program.cs
+++ ATM2.0/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace ATM2._0
 {
+    public class Banco : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuider)
+        {
+            optionsBuider.UseSqlServer(
+            @"Data Source=(localdb)\mssqllocaldb;" +
+            "Initial Catalog=Banco;" +
+            "Integrated Security=true;");
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Que onda");
+            using (var db = new Banco())
+            {
+                var newUser = new Usuario( 12345, 12345, "Pablo","Eduardo","Cardona","Fajardo");
+                db.Usuarios.Add(newUser);
+                db.SaveChanges();
+                Console.WriteLine("Usuario Creado");
+                foreach (var user in db.Usuarios)
+                {
+                    Console.WriteLine(user.pNombre);
+                }
+            }
+
         }
     }
 }
---
+++ ATM2.0/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ATM2._0
+{
+    public class Usuario
+    {
+        [Key]
+        public int nCuenta { get; set; }
+        [Required]
+        public int NIP { get; set; }
+
+        [Required]
+        public string pNombre { get; set; }
+        public string sNombre { get; set; }
+        [Required]
+        public string pApellido { get; set; }
+        public string sApellido { get; set; }
+
+        public Usuario( int nCuenta, int NIP, string pNombre, string sNombre, string pApellido, string sApellido)
+        {
+            this.nCuenta = nCuenta;
+            this.NIP = NIP;
+            this.pApellido = pApellido;
+            this.sApellido = sApellido;
+            this.pNombre = pNombre;
+            this.sNombre = sNombre;
+        }
+
+    }
+}
